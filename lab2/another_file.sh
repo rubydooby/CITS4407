0944bbc6acd6fa49bff04752438bebcaacbf7123
rubydooby <35957076+rubydooby@users.noreply.github.com>
2026-03-13 01:50:00 +0000

    added week2 files

---
+++ lab2/another_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/games/cowsay -f bud-frogs "Knock Knock"
+/usr/games/cowsay "Who's there"
+/usr/games/cowsay -f bud-frogs "Cows go"
+/usr/games/cowsay "Cows go who"
+/usr/games/cowsay -f bud-frogs "Cows don't go who they go moo!"
